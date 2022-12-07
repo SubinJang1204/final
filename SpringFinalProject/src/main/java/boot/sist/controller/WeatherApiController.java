3d7c0bf17ae7c7475b164428e23a7e787224de5d
minchan <minchan@naver.com>
2022-12-07 16:10:16 +0900

    221207 1610

--- SpringFinalProject/src/main/java/boot/sist/controller/WeatherApiController.java
+++ SpringFinalProject/src/main/java/boot/sist/controller/WeatherApiController.java
@@ -1,16 +1,28 @@
 package boot.sist.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WeatherApiController {
 
 	@GetMapping("/weather/weather")
-	public String restApiGetWeather() throws Exception {
+	public ModelAndView ApiGetWeather() throws Exception {
 		
+		ModelAndView mav = new ModelAndView();
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
 		
-		return "/weather/weather";
+		String date = sdf.format(new Date());
+		
+		mav.addObject("date", date);
+		
+		mav.setViewName("/weather/weather");
+		
+		return mav;
 	}
 }
